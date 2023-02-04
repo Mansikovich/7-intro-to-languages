552dc7936b3de0bd9a9ac66c8b418ad5c9a316b0
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-04 13:39:49 +0300

    2

---
+++ 50 task/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+{
+int[,] matrix = new int [3,4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+Console.Write("Enter element's value: ");
+int element=int.Parse(Console.ReadLine());
+
+if(FindElement(matrix, element))
+{
+    Console.WriteLine("Element found");
+}
+else
+{
+    Console.WriteLine("Element not found");
+}
+
+bool FindElement(int[,] matrix, int el)
+    {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+     for   (int j = 0; j < matrix.GetLength(1); j++)
+     {
+        if (matrix[i,j] == el) return true;
+     }
+        }
+        return false;
+    }
+
+void PrintArray(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+     {
+        Console.Write($"{matrix[i,j]} ");
+     }
+     Console.WriteLine();
+    }
+}    
+void FillArray(int[,] matrix)
+{
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+    {
+        matrix[i,j] = new Random().Next(1,100);
+    }
+    }
+}
+}
+}
+}
